13593bf87b0d08cff7d6c36468169ff1043e60e2
Rolf Poschmann <51391792+rolfposchmann@users.noreply.github.com>
2019-09-10 16:11:23 +0200

    Update merge_wireless.sh

--- usr/bin/merge_wireless.sh
+++ usr/bin/merge_wireless.sh
@@ -13,27 +13,44 @@
 #*/10 * * * * /usr/bin/merge_wireless.sh >/dev/null 2>&1
 
 cd /etc/config/
-oldfile = wireless_private_ap.old
-newfile = wireless_private_ap
 
-if [[ -f "$newfile" ]]; then
-  echo "$newfile exist"
+if [[ -f "wireless_private_ap" ]]; then
+  echo "wireless_private_ap exist"
 
   # Prevent file not found in if-statement
-  touch $oldfile
-
-  if diff $oldfile $newfile > /dev/null
+  touch  wireless_private_ap.old
+  
+  if diff wireless_private_ap wireless_private_ap.old > /dev/null
   then
     echo "No difference"
   else
-    echo "Difference"
+    echo "Difference: in private ap conf"
 
     # Merge files
     cat wireless_openwisp " " wireless_private_ap " " > wireless
     cat " " >> wireless
 
     #Save old file
-    mv wireless_private_ap wireless_private_ap.old
+    cp wireless_private_ap wireless_private_ap.old
+
+    wifi down && wifi up
+
+  fi
+else
+  if diff wireless_openwisp wireless > /dev/null
+  then
+    echo "No difference"
+  else
+    echo "Difference: wireless <-> wireless_openwisp"
+
+    cp wireless_openwisp wireless
+    wifi down && wifi up
+
+    if ! [[ -f "wireless_private_ap" ]]; then
+      echo "wirless_prive_ap not existing.. deleting old save file"
+      rm wireless_private_ap.old
+
+    fi
 
   fi
 fi
